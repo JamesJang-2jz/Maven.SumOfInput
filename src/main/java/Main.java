4a2800f8222caeb1325b9e7658b9e345c9d2d314
James <james@Zipcoders-MacBook-Pro-101.local>
2023-03-04 22:13:28 -0500

    james did extra creditos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,25 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
         
         int number = getInputNumber();
-        
+        long startTimeSum = System.currentTimeMillis();
         int sum = sumOfNumbers(number);
-        
+        long sumRunTime = System.currentTimeMillis() - startTimeSum;
         System.out.println(sum);
+        System.out.println("Run time for sunOfNumbers in milliseconds: " + sumRunTime);
+
+        long startTimeGauss = System.currentTimeMillis();
+        int gaussSum = gaussianSum(number);
+        long sumTimeGauss = System.currentTimeMillis() - startTimeGauss;
+        System.out.println(gaussSum);
+        System.out.println("Run time for gaussian formula in milliseconds: " + sumTimeGauss);
     }
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
@@ -20,7 +29,6 @@ public class Main {
             count+=i;
         } return count;
     }
-
     static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
@@ -32,14 +40,14 @@ public class Main {
         scan.close();
         return num;
     }
-    
-    
-    
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSum(int n) {
+        long startTime = System.currentTimeMillis();
+        int gaussSum = 0;
+        gaussSum = (n + 1) * (n/2);
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        return gaussSum;
     }
-   
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
     // use "System.currentTimeMillis()" to ask the system what time it is.
